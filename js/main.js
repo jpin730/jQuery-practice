9581ca633bde33b0a62e1f4c2120acb74dd3c7b7
Jaime Pineda <jaime.pineda.hqz@gmail.com>
2021-05-16 15:56:53 -0600

    :sparkles: show posts from JSON variable

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 $(document).ready(function () {
   $('.bxslider').bxSlider({
     mode: 'fade',
@@ -5,4 +7,53 @@ $(document).ready(function () {
     pause: 2000,
     auto: true,
   });
+});
+
+const posts = [
+  {
+    title: 'Title of the posts 1',
+    date: moment().format('ddd, MMMM Do YYYY'),
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio   nam fuga minus perspiciatis inventore earum illum atque ea maiores facere possimus, officiis soluta ut aliquid repellendus nesciunt quidem sapiente neque?',
+  },
+  {
+    title: 'Title of the posts 2',
+    date: moment().format('ddd, MMMM Do YYYY'),
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio   nam fuga minus perspiciatis inventore earum illum atque ea maiores facere possimus, officiis soluta ut aliquid repellendus nesciunt quidem sapiente neque?',
+  },
+  {
+    title: 'Title of the posts 3',
+    date: moment().format('ddd, MMMM Do YYYY'),
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio   nam fuga minus perspiciatis inventore earum illum atque ea maiores facere possimus, officiis soluta ut aliquid repellendus nesciunt quidem sapiente neque?',
+  },
+  {
+    title: 'Title of the posts 4',
+    date: moment().format('ddd, MMMM Do YYYY'),
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio   nam fuga minus perspiciatis inventore earum illum atque ea maiores facere possimus, officiis soluta ut aliquid repellendus nesciunt quidem sapiente neque?',
+  },
+  {
+    title: 'Title of the posts 5',
+    date: moment().format('ddd, MMMM Do YYYY'),
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio   nam fuga minus perspiciatis inventore earum illum atque ea maiores facere possimus, officiis soluta ut aliquid repellendus nesciunt quidem sapiente neque?',
+  },
+];
+
+const divPosts = $('div.posts');
+
+posts.forEach((post) => {
+  const template = `
+    <article class="post">
+      <h2>${post.title}</h2>
+      <small>Publication date: ${post.date}</small>
+      <p>
+        ${post.content}
+      </p>
+      <a href="#">Read more</a>
+    </article> `;
+
+  divPosts.append(template);
 });

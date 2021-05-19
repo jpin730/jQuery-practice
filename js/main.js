6486dcfc21c8842cfd5c0cebcf69e9725ac20467
Jaime Pineda <jaime.pineda.hqz@gmail.com>
2021-05-18 23:32:01 -0600

    :sparkles: add accordion on about page

--- js/main.js
+++ js/main.js
@@ -1,51 +1,52 @@
 'use strict';
 
-$(document).ready(function () {
-  $('.bxslider').bxSlider({
-    mode: 'fade',
-    captions: true,
-    pause: 2000,
-    auto: true,
+if (window.location.href.includes('index')) {
+  $(document).ready(function () {
+    $('.bxslider').bxSlider({
+      mode: 'fade',
+      captions: true,
+      pause: 2000,
+      auto: true,
+    });
   });
-});
 
-const posts = [
-  {
-    title: 'Title of the posts 1',
-    date: moment().format('ddd, MMMM Do YYYY'),
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio   nam fuga minus perspiciatis inventore earum illum atque ea maiores facere possimus, officiis soluta ut aliquid repellendus nesciunt quidem sapiente neque?',
-  },
-  {
-    title: 'Title of the posts 2',
-    date: moment().format('ddd, MMMM Do YYYY'),
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio   nam fuga minus perspiciatis inventore earum illum atque ea maiores facere possimus, officiis soluta ut aliquid repellendus nesciunt quidem sapiente neque?',
-  },
-  {
-    title: 'Title of the posts 3',
-    date: moment().format('ddd, MMMM Do YYYY'),
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio   nam fuga minus perspiciatis inventore earum illum atque ea maiores facere possimus, officiis soluta ut aliquid repellendus nesciunt quidem sapiente neque?',
-  },
-  {
-    title: 'Title of the posts 4',
-    date: moment().format('ddd, MMMM Do YYYY'),
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio   nam fuga minus perspiciatis inventore earum illum atque ea maiores facere possimus, officiis soluta ut aliquid repellendus nesciunt quidem sapiente neque?',
-  },
-  {
-    title: 'Title of the posts 5',
-    date: moment().format('ddd, MMMM Do YYYY'),
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio   nam fuga minus perspiciatis inventore earum illum atque ea maiores facere possimus, officiis soluta ut aliquid repellendus nesciunt quidem sapiente neque?',
-  },
-];
+  const posts = [
+    {
+      title: 'Title of the posts 1',
+      date: moment().format('ddd, MMMM Do YYYY'),
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio   nam fuga minus perspiciatis inventore earum illum atque ea maiores facere possimus, officiis soluta ut aliquid repellendus nesciunt quidem sapiente neque?',
+    },
+    {
+      title: 'Title of the posts 2',
+      date: moment().format('ddd, MMMM Do YYYY'),
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio   nam fuga minus perspiciatis inventore earum illum atque ea maiores facere possimus, officiis soluta ut aliquid repellendus nesciunt quidem sapiente neque?',
+    },
+    {
+      title: 'Title of the posts 3',
+      date: moment().format('ddd, MMMM Do YYYY'),
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio   nam fuga minus perspiciatis inventore earum illum atque ea maiores facere possimus, officiis soluta ut aliquid repellendus nesciunt quidem sapiente neque?',
+    },
+    {
+      title: 'Title of the posts 4',
+      date: moment().format('ddd, MMMM Do YYYY'),
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio   nam fuga minus perspiciatis inventore earum illum atque ea maiores facere possimus, officiis soluta ut aliquid repellendus nesciunt quidem sapiente neque?',
+    },
+    {
+      title: 'Title of the posts 5',
+      date: moment().format('ddd, MMMM Do YYYY'),
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio   nam fuga minus perspiciatis inventore earum illum atque ea maiores facere possimus, officiis soluta ut aliquid repellendus nesciunt quidem sapiente neque?',
+    },
+  ];
 
-const divPosts = $('div.posts');
+  const divPosts = $('div.posts');
 
-posts.forEach(function (post) {
-  const template = `
+  posts.forEach(function (post) {
+    const template = `
     <article class="post">
       <h2>${post.title}</h2>
       <small>Publication date: ${post.date}</small>
@@ -55,8 +56,9 @@ posts.forEach(function (post) {
       <a href="#">Read more</a>
     </article> `;
 
-  divPosts.append(template);
-});
+    divPosts.append(template);
+  });
+}
 
 const theme = $('#theme');
 
@@ -100,3 +102,7 @@ $('.login form').submit(function (event) {
   usernameInput.val('');
   whoamiParr.html(message).css('textAlign', 'center');
 });
+
+if (window.location.href.includes('about')) {
+  $('.accordion').accordion();
+}
